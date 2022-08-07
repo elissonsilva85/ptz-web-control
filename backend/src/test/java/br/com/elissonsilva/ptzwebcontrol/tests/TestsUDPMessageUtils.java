7c57c0189dedf9a3a0f4ab76c0e538f0dd530296
Elisson Silva <elisson.s@gmail.com>
2022-08-07 11:58:19 -0300

    #30 - JUnit tests classes for UDP and PTZ

---
+++ backend/src/test/java/br/com/elissonsilva/ptzwebcontrol/tests/TestsUDPMessageUtils.java
@@ -0,0 +1,83 @@
+package br.com.elissonsilva.ptzwebcontrol.tests;
+
+
+import br.com.elissonsilva.ptzwebcontrol.backend.udp.UdpMessageUtils;
+import org.junit.Test;
+
+import static br.com.elissonsilva.ptzwebcontrol.backend.udp.UdpMessageUtils.*;
+import static org.junit.Assert.*;
+
+public class TestsUDPMessageUtils {
+
+    @Test
+    public void whenSpeedConvertedReceiveWrongReceiveInterval() {
+
+        Exception exception = assertThrows(Exception.class, () ->
+            speedConverter(0,0,0,0,0)
+        );
+
+        String expectedMessage = "receivedMax <= receivedMin";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void whenSpeedConvertedReceiveWrongOutputInterval() {
+
+        Exception exception = assertThrows(Exception.class, () ->
+            speedConverter(0,0,10,0,0)
+        );
+
+        String expectedMessage = "outputMax <= outputMin";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void whenSpeedConvertedReceiveNegativeReceiveInterval() throws Exception {
+
+        int expectedOutput = 25;
+        int actualOutput = UdpMessageUtils.speedConverter(-5,-10,10,0,100);
+
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void whenSpeedConvertedReceiveNegativeOutputInterval() throws Exception {
+
+        int expectedOutput = -5;
+        int actualOutput = UdpMessageUtils.speedConverter(5,0,20,-10,10);
+
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void whenSpeedConvertedReceivePositiveIntervals() throws Exception {
+
+        int expectedOutput = 3;
+        int actualOutput = UdpMessageUtils.speedConverter(5,0,20,0,10);
+
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void whenSpeedConvertedReceiveLowerLimit() throws Exception {
+
+        int expectedOutput = 10;
+        int actualOutput = UdpMessageUtils.speedConverter(1,0,100,10,20);
+
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void whenSpeedConvertedReceiveUpperLimit() throws Exception {
+
+        int expectedOutput = 20;
+        int actualOutput = UdpMessageUtils.speedConverter(99,0,100,10,20);
+
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
---
+++ backend/src/test/java/br/com/elissonsilva/ptzwebcontrol/tests/TestsUDPServerSimple.java
@@ -0,0 +1,84 @@
+package br.com.elissonsilva.ptzwebcontrol.tests;
+
+import br.com.elissonsilva.ptzwebcontrol.backend.services.UDPServerService;
+import br.com.elissonsilva.ptzwebcontrol.backend.udp.UdpMessageUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestsUDPServerSimple {
+
+    public static class EchoClient {
+        private final DatagramSocket socket;
+        private final InetAddress address;
+
+        private byte[] buf;
+
+        public EchoClient() throws SocketException, UnknownHostException {
+            socket = new DatagramSocket();
+            address = InetAddress.getByName("localhost");
+        }
+
+        public String sendEcho(String msg) {
+            System.out.println("sendEcho: ------------------------");
+            System.out.println("sendEcho: msg [" + msg + "]");
+            byte[] buf = new byte[256];
+            DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 4445);
+            //
+            byte[] tmp = UdpMessageUtils.hexStringToBytes(msg);
+            packet.setData(tmp, 0, tmp.length);
+            //
+            try {
+                socket.send(packet);
+                System.out.println("sendEcho: send");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            //
+            packet = new DatagramPacket(buf, buf.length);
+            try {
+                socket.receive(packet);
+                System.out.println("sendEcho: receive");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            //
+            String received = UdpMessageUtils.bytesToHex(packet.getData(), packet.getLength());
+            System.out.println("sendEcho: msg [" + received + "]");
+            return received;
+        }
+
+        public void close() {
+            socket.close();
+        }
+    }
+
+    EchoClient client;
+
+    @Before
+    public void setup() throws SocketException, UnknownHostException {
+        new UDPServerService(null,"PTZ", 4445).start();
+        client = new EchoClient();
+    }
+
+    @Test
+    public void whenCanSendAndReceivePacket_thenCorrect() {
+        String echo = client.sendEcho("81090612FF");
+        assertEquals("9050" + "00000000" + "00000000" + "FF", echo);
+        echo = client.sendEcho("81090447FF");
+        assertEquals("9050" + "00000000" + "FF", echo);
+        echo = client.sendEcho("8101040733FF");
+        assertEquals("", echo);
+    }
+
+    @After
+    public void tearDown() {
+        client.sendEcho("FF");
+        client.close();
+    }
+}
